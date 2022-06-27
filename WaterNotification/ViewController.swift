dc8c924f0745264792cdd19ad503c0e1b4642ad2
Exempti0n <sj990927@gmail.com>
2022-06-28 01:01:26 +0900

    Initial Commit

---
+++ WaterNotification/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WaterNotification
+//
+//  Created by 오승준 on 2022/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
